5256903ecb98ae4887a38da8feb9c90c7d57eb27
Yaroslav Shabelnyk <shabelnyk63@gmail.com>
2022-11-17 21:14:25 +0200

    V 0.2
    Added api key save

--- services/storage.service.js
+++ services/storage.service.js
@@ -1,9 +1,34 @@
 import {homedir} from "os";
 import {join} from "path";
+import {promises} from "fs";
 
-const filePath = join(homedir(), "../weather-data.json")
+const filePath = join(homedir(), "/Documents/weather-data.json")
 
-const saveKeyValue = (key, value) => {
+const saveKeyValue = async (key, value) => {
+    var data = {};
+    if (await isExsist(filePath)) {
+        const file = await promises.readFile(filePath)
+        data = JSON.parse(file);
+    }
+    data[key] = value;
+    await promises.writeFile(filePath, JSON.stringify(data))
+}
+const getKeyValue = async (key) => {
+    if (await isExsist(filePath)){
+        const file = await promises.readFile(filePath);
+        const data = JSON.parse(file);
+        return data[key];
+    }
+    return undefined;
 }
 
-export {saveKeyValue}
+const isExsist = async (path) => {
+    try{
+        await promises.stat(path);
+        return true;
+    } catch (e) {
+        return false;
+    }
+}
+
+export {saveKeyValue, getKeyValue}
